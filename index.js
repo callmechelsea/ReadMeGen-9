dc327303795210fd90cd40066a991ffce483635d
callmechelsea <chelseakathrynp@gmail.com>
2023-07-06 18:54:11 -0700

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('path')
+const fs = require('fs')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description about your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command needs to be run to test?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to repo?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'], 
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
